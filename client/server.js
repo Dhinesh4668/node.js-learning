df797911d80d09256681ae5e0ab4d09da26df6d4
Dhinesh Kumar <dhinesh4668@gmail.com>
2023-01-07 23:04:36 +0530

    10th commit

--- client/server.js
+++ client/server.js
@@ -1,9 +1,24 @@
-const http = require('http')
+const http = require('http');
+
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log("server was created");
+    console.log(req.url, req.method);
+    res.setHeader('content-type', 'html')
+    fs.readFile('./index.html', 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.write(data)
+            res.end();
+        }
+    })
+
 })
 
-server.listen(3000, 'localhost', ()=>{
-    console.log('this is the listener');
+server.listen(5000, 'localhost', () => {
+    console.log('listning the localhost:3000');
 })
+
+
+// create browser req and response
