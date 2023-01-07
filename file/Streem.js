53498874d13b670e0a851b9db071434a7da7e45a
Dhinesh Kumar <dhinesh4668@gmail.com>
2023-01-07 23:47:58 +0530

    commit

--- file/Streem.js
+++ file/Streem.js
@@ -1,9 +1,16 @@
 const fs = require('fs') 
 
 const readStream = fs.createReadStream('../data/readfile.txt', {encoding : 'utf-8'});
+const WriteStreem = fs.createWriteStream('../data/weiteStreem.txt')
 
 readStream.on('data', (chunk)=>{
     console.log('\t====gost=====');
     console.log("======this is dhinesh=======");
     console.log(chunk);
+    WriteStreem('\n hai dhiensh')
+    WriteStreem.write(chunk);
+
+    console.log(chunk);
 });
+
+readStream.pipe(WriteStreem)
